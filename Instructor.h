83ac85e655e724d43c93051df47b36c1bb042426
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-10-28 20:44:49 +0300

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -3,6 +3,31 @@
 
 #include "Person.h"
 
-//define class instructor here
+class Instructor : public Person {
+private:
+    string department;
+    int experienceYears;
+
+public:
+    // Default constructor
+    Instructor();
+    
+    // Parameterized constructor
+    Instructor(string n, int i, string dept, int exp);
+    
+    // Destructor
+    ~Instructor();
+    
+    // Setters
+    void setDepartment(string dept);
+    void setExperienceYears(int exp);
+    
+    // Getters
+    string getDepartment() const;
+    int getExperienceYears() const;
+    
+    // Override display method
+    void display() const override;
+};
 
 #endif
