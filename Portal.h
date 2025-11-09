e3497a01efdb83661e9401bc4937a5f5234c6f36
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-11-09 21:59:59 +0200

    Refactor Portal class to remove implementation details
    Removed the implementation details of the Portal class, keeping only the class declaration.

--- Portal.h
+++ Portal.h
@@ -5,42 +5,21 @@
 #include "Instructor.h"
 #include "Course.h"
 
-class Portal {
-private:
-    Student** students;
-    Instructor** instructors;
-    Course** courses;
-    int studentCount;
-    int instructorCount;
-    int courseCount;
-    int studentCapacity;
-    int instructorCapacity;
-    int courseCapacity;
+//#write portal class here
 
-public:
-    // Default constructor
-    Portal();
-    
-    // Parameterized constructor
-    Portal(int studCap, int instCap, int courCap);
-    
-    // Destructor
-    ~Portal();
-    
-    // Methods
-    void initializePortal();
-    bool enrollStudentInCourse(Student* s, Course* c);
-    void displayPortalInfo() const;
-    
-    // Helper methods to add entities
-    bool addStudent(Student* s);
-    bool addInstructor(Instructor* i);
-    bool addCourse(Course* c);
-    
-    // Getters
-    int getStudentCount() const;
-    int getInstructorCount() const;
-    int getCourseCount() const;
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
