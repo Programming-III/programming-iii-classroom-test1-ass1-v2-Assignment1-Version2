0e7326a922835ba4d61a39cd2a9e45fcaf0684e5
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-11-09 22:00:35 +0200

    Remove Student class definition
    Removed the Student class definition and its methods.

--- Student.h
+++ Student.h
@@ -3,31 +3,18 @@
 
 #include "Person.h"
 
-class Student : public Person {
-private:
-    int yearLevel;
-    string major;
+//#write student class here
 
-public:
-    // Default constructor
-    Student();
-    
-    // Parameterized constructor
-    Student(string n, int i, int year, string maj);
-    
-    // Destructor
-    ~Student();
-    
-    // Setters
-    void setYearLevel(int year);
-    void setMajor(string maj);
-    
-    // Getters
-    int getYearLevel() const;
-    string getMajor() const;
-    
-    // Override display method
-    void display() const override;
-};
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
