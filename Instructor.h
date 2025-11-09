32c4cd37c0d31864e6ffea5c2bae331b19420764
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-11-09 21:58:17 +0200

    Remove Instructor class implementation
    Removed the implementation of the Instructor class and its methods.

--- Instructor.h
+++ Instructor.h
@@ -3,31 +3,17 @@
 
 #include "Person.h"
 
-class Instructor : public Person {
-private:
-    string department;
-    int experienceYears;
+//#write Instructor class here
 
-public:
-    // Default constructor
-    Instructor();
-    
-    // Parameterized constructor
-    Instructor(string n, int i, string dept, int exp);
-    
-    // Destructor
-    ~Instructor();
-    
-    // Setters
-    void setDepartment(string dept);
-    void setExperienceYears(int exp);
-    
-    // Getters
-    string getDepartment() const;
-    int getExperienceYears() const;
-    
-    // Override display method
-    void display() const override;
-};
+
+
+
+
+
+
+
+
+
+
 
 #endif
