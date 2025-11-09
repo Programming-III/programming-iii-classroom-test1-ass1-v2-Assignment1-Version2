5bc56a2f8caf2af3272779ca082fa405bc985450
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-11-09 21:58:55 +0200

    Remove Person class implementation details
    Removed the implementation details of the Person class.

--- Person.h
+++ Person.h
@@ -4,31 +4,17 @@
 #include <string>
 using namespace std;
 
-class Person {
-private:
-    string name;
-    int id;
+//#write Person class here
 
-public:
-    // Default constructor
-    Person();
-    
-    // Parameterized constructor
-    Person(string n, int i);
-    
-    // Destructor
-    ~Person();
-    
-    // Setters
-    void setName(string n);
-    void setId(int i);
-    
-    // Getters
-    string getName() const;
-    int getId() const;
-    
-    // Display method
-    virtual void display() const;
-};
+
+
+
+
+
+
+
+
+
+
 
 #endif
